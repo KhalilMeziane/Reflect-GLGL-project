257089b69e8e4757fa847643e928d3798c44dc03
MezianeKhalil <khalilmez2000@gmail.com>
2021-12-15 15:05:23 +0100

    build profile page & edit authNavbar

--- src/components/authNavbar.jsx
+++ src/components/authNavbar.jsx
@@ -1,108 +1,74 @@
 import React from "react";
-
 import {
   chakra,
-  Box,
   Flex,
-  useColorModeValue,
-  VisuallyHidden,
   HStack,
-  Button,
-  useDisclosure,
   VStack,
-  IconButton,
-  CloseButton,
-  Avatar,
+  Text,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
 } from "@chakra-ui/react"
-import {
-  AiOutlineMenu,
-  AiFillHome,
-} from "react-icons/ai"
-import { BsPlus } from "react-icons/bs"
+import { BiCaretDown , BiLogOut, BiUser, BiHomeAlt } from "react-icons/bi";
+import { Link } from 'react-router-dom'
 
 export default function Dsll() {
-  const bg = useColorModeValue("white", "gray.800");
-  const mobileNav = useDisclosure();
 
   return (
-    <React.Fragment>
+    <>
       <chakra.header
-        bg={bg}
         w="full"
-        px={{ base: 2, sm: 4 }}
         py={4}
-        shadow="md"
+        shadow="xs"
+        px={{ base: 4, sm: 6, md: 8, xl: 28 }}
       >
         <Flex alignItems="center" justifyContent="space-between" mx="auto">
           <HStack display="flex" spacing={3} alignItems="center">
-            <Box display={{ base: "inline-flex", md: "none" }}>
-              <IconButton
-                display={{ base: "flex", md: "none" }}
-                aria-label="Open menu"
-                fontSize="20px"
-                color={useColorModeValue("gray.800", "inherit")}
-                variant="ghost"
-                icon={<AiOutlineMenu />}
-                onClick={mobileNav.onOpen}
-              />
-              <VStack
-                pos="absolute"
-                top={0}
-                left={0}
-                right={0}
-                display={mobileNav.isOpen ? "flex" : "none"}
-                flexDirection="column"
-                p={2}
-                pb={4}
-                m={2}
-                bg={bg}
-                spacing={3}
-                rounded="sm"
-                shadow="sm"
-              >
-                <CloseButton
-                  aria-label="Close menu"
-                  justifySelf="self-start"
-                  onClick={mobileNav.onClose}
-                />
-                <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
-                  Dashboard
-                </Button>
-              </VStack>
-            </Box>
-            <chakra.a
-              href="/"
-              title="Choc Home Page"
-              display="flex"
-              alignItems="center"
-            >
-              {/* <Logo /> */}
-              <VisuallyHidden>Choc</VisuallyHidden>
-            </chakra.a>
-
-            <HStack spacing={3} display={{ base: "none", md: "inline-flex" }}>
-              <Button variant="ghost" leftIcon={<AiFillHome />} size="sm">
-                Dashboard
-              </Button>
-            </HStack>
+              <Text fontSize="2xl" fontWeight="semibold">
+                <Link to="/dashboard">Reflect</Link>
+              </Text>
           </HStack>
           <HStack
-            spacing={3}
-            display={mobileNav.isOpen ? "none" : "flex"}
+            spacing={1}
             alignItems="center"
           >
-            <Button colorScheme="green" leftIcon={<BsPlus />}>
-              New Project
-            </Button>
-            <Avatar
-              size="md"
-              name="Dan Abrahmov"
-              src="https://bit.ly/dan-abramov"
-            />
+            <Popover>
+              <PopoverTrigger>
+                <Button variant="none" outline={"none"} rightIcon={<BiCaretDown />} fontWeight="normal" textTransform="capitalize">khalil meziane</Button>
+              </PopoverTrigger>
+              <PopoverContent w="44">
+                <PopoverArrow />
+                <PopoverBody>
+                  <VStack alignItems={"start"}>
+                    <Link to="/dashboard">
+                      <Flex gap="2" justifyItems={"center"} alignItems={"center"}>
+                        <BiHomeAlt/>
+                        <Text fontSize="lg" textTransform="capitalize">dashboard</Text>
+                      </Flex>
+                    </Link>
+                    <Link to="/profile">
+                      <Flex gap="2" justifyItems={"center"} alignItems={"center"}>
+                        <BiUser/>
+                        <Text fontSize="lg" textTransform="capitalize">profile</Text>
+                      </Flex>
+                    </Link>
+                    <Link to="/logout">
+                      <Flex gap="2" justifyItems={"center"} alignItems={"center"}>
+                        <BiLogOut/>
+                        <Text fontSize="lg" textTransform="capitalize">logout</Text>
+                      </Flex>
+                    </Link>
+                  </VStack>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </HStack>
         </Flex>
       </chakra.header>
-    </React.Fragment>
+    </>
   );
 }
 
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Helmet } from "react-helmet"
 import { AuthNavbar } from '../components/_index'
 // import { Link } from 'react-router-dom'
+import { Box, Button, Flex, Input, SimpleGrid, Stack, Text } from '@chakra-ui/react';
 
 export default function profile() {
     return (
@@ -10,7 +11,86 @@ export default function profile() {
                 <title>Account | Reflect</title>
             </Helmet>
             <AuthNavbar/>
-
+            <Box 
+                border="2px" 
+                borderStyle="solid" 
+                borderColor="gray.200"
+                rounded="md"
+                mt="8"
+                mx="auto"
+                w={{base: 11/12, lg:10/12}}
+            >
+                {/* <Text as="h1" fontWeight="semibold" fontSize="3xl">Account</Text> */}
+                <Box>
+                    <Text 
+                        color="gray.900" 
+                        fontSize="2xl" 
+                        p="5"
+                        borderBottom="2px" 
+                        borderStyle="solid" 
+                        borderColor="gray.200"
+                    >Personal info</Text>
+                    <Box p="5">
+                        <Text 
+                            color="gray.700" 
+                            fontSize="md"
+                            mb="4" 
+                        >
+                            Your email
+                        </Text>
+                        <Text 
+                            color="gray.900" 
+                            fontSize="xl" 
+                            fontWeight="semibold"
+                        >
+                            khalilmez2000@gmail.com
+                        </Text>
+                    </Box>
+                    <hr/>
+                    <SimpleGrid
+                        columns={1}
+                        p={5}
+                        spacing={4}
+                    >
+                        <Stack>
+                            <Text mb='3px' textTransform="capitalize">email address</Text>
+                            <Input
+                                type="email"
+                                placeholder="exampel@gmail.com"
+                                required="true"
+                                color="gray.900"
+                                borderColor="gray.300"
+                                border="2px"
+                                borderStyle="solid"
+                                size="lg"
+                                w={{base: "full", lg:5/12}}
+                            />
+                        </Stack>
+                        <Stack>
+                            <Text mb='3px' textTransform="capitalize">full name</Text>
+                            <Input
+                                type="text"
+                                placeholder="john snow"
+                                required="true"
+                                color="gray.900"
+                                borderColor="gray.300"
+                                border="2px"
+                                borderStyle="solid"
+                                size="lg"
+                                w={{base: "full", lg:5/12}}
+                            />
+                        </Stack>
+                       <Flex gap="2">
+                            <Button bg="green.500" fontSize="lg" textTransform="capitalize" variant="none" color="white"  py={4} type="submit" px="10" fontWeight="normal">
+                                save
+                            </Button>
+                            <Button border="2px" color="green.500"  borderStyle="solid" borderColor="green.500" fontSize="lg" variant="none" px="10" textTransform="capitalize" fontWeight="normal"  py={4} type="submit">
+                                cancel
+                            </Button>
+                       </Flex>
+                    </SimpleGrid>
+                </Box>
+            </Box>
         </>
     )
 }
