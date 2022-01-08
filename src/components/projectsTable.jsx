8a848772340d051435d437b00575a50c01c28676
MezianeKhalil <khalilmez2000@gmail.com>
2022-01-08 18:16:31 +0100

    add 404 content for project page

--- src/components/projectsTable.jsx
+++ src/components/projectsTable.jsx
@@ -8,7 +8,7 @@ import {
   Tr,
   Th,
   Td,
-  Tbody,
+  Tbody
 } from '@chakra-ui/react'
 import { BsBoxArrowUpRight } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
@@ -27,165 +27,78 @@ export default function ProjectsTable() {
         alignItems="center"
         justifyContent="center"
       >
-        <Table
-            w="full"
-            bg="white"
-            shadow="lg"
-            display={{
-              base: 'block',
-              md: 'table',
-            }}
-            sx={{
-              '@media print': {
-                display: 'table',
-              }
-            }}
-        >
-          <Thead
-            display={{
-              base: 'none',
-              md: 'table-header-group',
-            }}
-            sx={{
-              '@media print': {
-                display: 'table-header-group',
-              },
-            }}
+        {
+          projects && 
+          <Table
+              w="full"
+              bg="white"
+              shadow="lg"
+              display={{
+                base: 'block',
+                md: 'table',
+              }}
+              sx={{
+                '@media print': {
+                  display: 'table',
+                }
+              }}
           >
-            <Tr>
-              {header.map((x) => (
-                <Th bg="gray.100" fontFamily="poppins" fontWeight="semibold" color="gray.800" fontSize="md" key={x}>{x}</Th>
-              ))}
-            </Tr>
-          </Thead>
-          <Tbody
-            display={{
-              base: 'block',
-              lg: 'table-row-group',
-            }}
-            sx={{
-              '@media print': {
-                display: 'table-row-group',
-              },
-            }}
-          >
+            <Thead
+              display={{
+                base: 'none',
+                md: 'table-header-group',
+              }}
+              sx={{
+                '@media print': {
+                  display: 'table-header-group',
+                },
+              }}
+            >
+              <Tr>
+                {header.map((x) => (
+                  <Th bg="gray.100" fontFamily="poppins" fontWeight="semibold" color="gray.800" fontSize="md" key={x}>{x}</Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody
+              display={{
+                base: 'block',
+                lg: 'table-row-group',
+              }}
+              sx={{
+                '@media print': {
+                  display: 'table-row-group',
+                },
+              }}
+            >
 
-            {
-              projectList?.map(({name,id})=>{
-                return(
-                <React.Fragment key={id}>
-                  <Tr>
-                    <Td textTransform="uppercase" color="gray.900" fontWeight={"medium"}>
-                      {name}
-                    </Td>
-                    <Td>
-                      <ButtonGroup variant="solid" size="sm" spacing={3}>
-                        <Link to={`/dashboard/${id}`}>
-                          <IconButton
-                            colorScheme="blue"
-                            icon={<BsBoxArrowUpRight />}
-                          />
-                        </Link>
-                        <DeleteProject id={id} name={name}/>
-                      </ButtonGroup>
-                    </Td>
-                  </Tr>
-                </React.Fragment> 
-                )
-              }) 
-            }
-          </Tbody>
-        </Table>
+              {
+                projectList?.map(({name,id})=>{
+                  return(
+                  <React.Fragment key={id}>
+                    <Tr>
+                      <Td textTransform="uppercase" color="gray.900" fontWeight={"medium"}>
+                        {name}
+                      </Td>
+                      <Td>
+                        <ButtonGroup variant="solid" size="sm" spacing={3}>
+                          <Link to={`/dashboard/${id}`}>
+                            <IconButton
+                              colorScheme="blue"
+                              icon={<BsBoxArrowUpRight />}
+                            />
+                          </Link>
+                          <DeleteProject id={id} name={name}/>
+                        </ButtonGroup>
+                      </Td>
+                    </Tr>
+                  </React.Fragment> 
+                  )
+                }) 
+              }
+            </Tbody>
+          </Table>
+        }
       </Flex>
     )
 }
-
-/*
-
-              {data.map((token, tid) => {
-              return (
-                <Tr
-                  key={tid}
-                  display={{
-                    base: 'grid',
-                    md: 'table-row',
-                  }}
-                  sx={{
-                    '@media print': {
-                      display: 'table-row',
-                    },
-                    gridTemplateColumns: 'minmax(0px, 35%) minmax(0px, 65%)',
-                    gridGap: '10px',
-                  }}
-                >
-                  {Object.keys(token).map((x) => {
-                    return (
-                      <React.Fragment key={`${tid}${x}`}>
-                        <Td
-                          display={{
-                            base: 'table-cell',
-                            md: 'none',
-                          }}
-                          sx={{
-                            '@media print': {
-                              display: 'none',
-                            },
-                            textTransform: 'uppercase',
-                            color:"black",
-                            fontWeight: 'bold',
-                          }}
-                        >
-                          {x}
-                        </Td>
-                        <Td
-                          color="gray.900"
-                          fontSize="md"
-                        >
-                          {token[x]}
-                        </Td>
-                      </React.Fragment>
-                    );
-                  })}
-                  <Td
-                    display={{
-                      base: 'table-cell',
-                      md: 'none',
-                    }}
-                    sx={{
-                      '@media print': {
-                        display: 'none',
-                      },
-                      textTransform: 'uppercase',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    Actions
-                  </Td>
-                  <Td
-                    display={{
-                      base: 'table-cell',
-                      md: 'block',
-                    }}
-                    sx={{
-                      '@media print': {
-                        display: 'none',
-                      },
-                      textTransform: 'uppercase',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    <ButtonGroup variant="solid" size="sm" spacing={3}>
-                      <Link to="/dashboard/5">
-                        <IconButton
-                          colorScheme="blue"
-                          icon={<BsBoxArrowUpRight />}
-                        />
-                      </Link>
-                      <DeleteProject/>
-                    </ButtonGroup>
-                  </Td>
-                </Tr>
-              )
-            })}
-
-*/
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 import { AuthNavbar } from '../components/_index'
-import { Box, Text, Divider, Tabs, Tab,  TabPanels, TabPanel, TabList, Flex } from '@chakra-ui/react'
+import { Box, Text, Divider, Tabs, Tab,  TabPanels, TabPanel, TabList, Flex, Heading, Image } from '@chakra-ui/react'
 import { EditProject } from '../components/modals/_index'
 import Pert from '../components/charts/pert/pert'
 import { useParams } from 'react-router-dom'
 import { useProject } from '../hooks/useProject'
+import notFound from '../assets/images/404-Not-Found.svg'
+import { Link } from 'react-router-dom'
 
 export default function Project() {
     const { id } = useParams()
@@ -18,26 +20,52 @@ export default function Project() {
             </Helmet>
             <AuthNavbar/>
             <Box px={{ base: 4, sm: 6, md: 8, xl: 28 }} py="3">
-                <Flex justify={"space-between"} align={"center"}>
-                    <Text py="3" fontSize="2xl" textTransform={"capitalize"} fontWeight={"semibold"}>{project?.data?.project?.name}</Text>
-                    <EditProject project={project?.data?.project}/>
-                </Flex>
-                <Divider bg="black" my="3"/>
-                <Tabs variant='enclosed'>
-                    <TabList>
-                        <Tab>Pert Chart</Tab>
-                        <Tab>Gant Chart</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <Pert tasks={project?.data?.project?.tasks}/>
-                        </TabPanel>
-                        <TabPanel>
-                            <p>this is gant chart!!</p>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
+                {
+                    project &&
+                    <>
+                        <Flex justify={"space-between"} align={"center"}>
+                            <Text py="3" fontSize="2xl" textTransform={"capitalize"} fontWeight={"semibold"}>{project?.data?.project?.name}</Text>
+                            <EditProject project={project?.data?.project}/>
+                        </Flex>
+                        <Divider bg="black" my="3"/>
+                    </>
+                }
+                {
+                    !project? <NotFound/>: <Content project={project}/>
+                }
             </Box>
         </>
+    )
+}
+
+const NotFound=()=>{
+    return(
+        <>
+            <Box boxSize='lg' mx="auto" mt="-12">
+                <Image src={notFound}/>
+            </Box>
+            <Heading as="H1" size="xl" mt="-32" textAlign={"center"} fontFamily={"poppins"} fontWeight={"medium"}>Project Not Found</Heading>
+            <Text textAlign={"center"} fontSize={"xl"} color="green.500" mt="4" textTransform={"capitalize"} fontWeight={"medium"}>
+                <Link to="/dashboard">go back</Link>
+            </Text>
+        </>
+    )
+}
+const Content = ({project})=>{
+    return(
+        <Tabs variant='enclosed'>
+            <TabList>
+                <Tab>Pert Chart</Tab>
+                <Tab>Gant Chart</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Pert tasks={project?.data?.project?.tasks}/>
+                </TabPanel>
+                <TabPanel>
+                    <p>this is gant chart!!</p>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     )
 }
