9334aa66ecec2163e60f36b73934838890bbfcac
MezianeKhalil <khalilmez2000@gmail.com>
2022-01-08 17:43:18 +0100

    build 404 page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
-
+import { Heading, Image, Box, Text  } from "@chakra-ui/react"
+import notFound from '../assets/images/404-Not-Found.svg'
+import { Link } from 'react-router-dom'
 export default function NotFound() {
     return (
         <>
         <Helmet>
                 <title>404 | Reflect</title>
             </Helmet>
-            <h1>404 NotFound</h1>
+            <Box boxSize='xl' mx="auto">
+                <Image src={notFound}/>
+            </Box>
+            <Heading as="H1" size="2xl" mt="-48" textAlign={"center"} fontFamily={"poppins"} fontWeight={"medium"}>Page Not Found</Heading>
+            <Text textAlign={"center"} fontSize={"xl"} color="green.500" mt="4" textTransform={"capitalize"} fontWeight={"medium"}>
+                <Link to="/">go back</Link>
+            </Text>
         </>
     )
 }
